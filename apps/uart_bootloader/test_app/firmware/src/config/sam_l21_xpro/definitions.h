8ad7037d9c320afc56393751eee7aa4184e042fa
Amit Raddi <amit.raddi@microchip.com>
2019-05-15 19:22:13 +0530

    [BOOTLOADER] Regenerated and Tested uart_bootloader_test_app_sam_l21_xpro
    MH3-12659
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
@@ -52,11 +52,11 @@
 #include "peripheral/sercom/usart/plib_sercom3_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
+#include "peripheral/pm/plib_pm.h"
 #include "bsp/bsp.h"
 #include "app.h"
 
