33154921d32bc0be958b3c6f4d194c06078b362d
Amit Raddi <amit.raddi@microchip.com>
2019-10-01 15:18:37 +0530

    [BOOTLOADER] Regenerated and tested uart_bootloader_sam_l21_xpro
    MH3-22668
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/bootloader/firmware/src/config/sam_l21_xpro/initialization.c
+++ apps/uart_bootloader/bootloader/firmware/src/config/sam_l21_xpro/initialization.c
@@ -96,6 +96,8 @@ void SYS_Initialize ( void* data )
 
     SERCOM3_USART_Initialize();
 
+    NVMCTRL_Initialize( );
+
     EVSYS_Initialize();
 
 	SYSTICK_TimerInitialize();
--- apps/uart_bootloader/bootloader/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.h
+++ apps/uart_bootloader/bootloader/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.h
@@ -66,14 +66,14 @@
 // *****************************************************************************
 
 
-/*** Macros for GPIO_PA02 pin ***/
-#define GPIO_PA02_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 2)
-#define GPIO_PA02_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 2)
-#define GPIO_PA02_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 2)
-#define GPIO_PA02_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 2)) & 0x01)
-#define GPIO_PA02_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 2)
-#define GPIO_PA02_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 2)
-#define GPIO_PA02_PIN                  PORT_PIN_PA02
+/*** Macros for SWITCH pin ***/
+#define SWITCH_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 2)
+#define SWITCH_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 2)
+#define SWITCH_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 2)
+#define SWITCH_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 2)) & 0x01)
+#define SWITCH_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 2)
+#define SWITCH_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 2)
+#define SWITCH_PIN                  PORT_PIN_PA02
 
 
 
