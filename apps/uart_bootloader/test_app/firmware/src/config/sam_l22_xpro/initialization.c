290425545807641d626f0f2af2244bae5207bf89
Amit Raddi <amit.raddi@microchip.com>
2019-10-01 15:23:44 +0530

    [BOOTLOADER] Regenerated and tested uart_bootloader_test_app_sam_l22_xpro
    MH3-22704
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_l22_xpro/initialization.c
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_l22_xpro/initialization.c
@@ -100,7 +100,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
     PM_Initialize();
 
@@ -108,6 +108,8 @@ void SYS_Initialize ( void* data )
     PORT_Initialize();
 
     CLOCK_Initialize();
+
+    NVMCTRL_Initialize( );
 
     EVSYS_Initialize();
 
