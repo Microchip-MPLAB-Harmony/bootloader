d4a4e5c219875baa131ea670da63a78025d1693b
Amit Raddi <amit.raddi@microchip.com>
2019-10-03 17:20:33 +0530

    [BOOTLOADER] Regenerated and tested uart_bootloader_test_app_pic32mk_mcj_curiosity_pro
    MH3-22674
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/pic32mk_mcj_curiosity_pro/user.h
+++ apps/uart_bootloader/test_app/firmware/src/config/pic32mk_mcj_curiosity_pro/user.h
@@ -16,6 +16,31 @@
 
 *******************************************************************************/
 
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
 #ifndef USER_H
 #define USER_H
 
@@ -46,6 +71,8 @@ extern "C" {
 #define APP_TIMER_DelayMs   CORETIMER_DelayMs
 
 #define BTL_TRIGGER_RAM_START   KVA0_TO_KVA1(0x80000000)
+
+#define DCACHE_CLEAN_BY_ADDR(start, sz)
 
 static inline void APP_SystemReset( void )
 {
