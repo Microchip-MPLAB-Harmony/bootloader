977e245c222bc072450abee5aeb69f2bc76651bd
Amit Raddi <amit.raddi@microchip.com>
2019-10-03 15:13:39 +0530

    [BOOTLOADER] Regenerated and tested uart_bootloader_test_app_sam_e54_xpro
    MH3-22694
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_e54_xpro/tasks.c
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_e54_xpro/tasks.c
@@ -54,6 +54,7 @@
 #include "definitions.h"
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -67,7 +68,6 @@
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -86,7 +86,6 @@ void SYS_Tasks ( void )
 
 
 }
-
 
 /*******************************************************************************
  End of File
