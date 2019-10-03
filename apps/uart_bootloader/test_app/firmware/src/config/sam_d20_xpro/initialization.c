fe059b3fd8bc9152e69fee38808972ea04867c43
Amit Raddi <amit.raddi@microchip.com>
2019-10-03 14:20:28 +0530

    [BOOTLOADER] Regenerated and tested uart_bootloader_test_app_sam_d20_xpro
    MH3-22688
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_d20_xpro/initialization.c
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_d20_xpro/initialization.c
@@ -100,7 +100,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
     PORT_Initialize();
@@ -109,6 +109,8 @@ void SYS_Initialize ( void* data )
 
 
     SERCOM3_USART_Initialize();
+
+    NVMCTRL_Initialize( );
 
     EVSYS_Initialize();
 
