ad3983cd211bdd2f61d92b8c1f05efd78535d700
Amit Raddi <amit.raddi@microchip.com>
2020-01-27 08:58:12 +0530

    [UART] Added uart_fail_safe_bootloader_pi32mzt_app_ef_sk application configuration
    MH3-28795
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

---
+++ apps/uart_fail_safe_bootloader/test_app/firmware/src/app_mips.c
@@ -0,0 +1,203 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app_mips.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app_mips.h"
+#include "definitions.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+#define BTL_TRIGGER_PATTERN (0x5048434DUL)
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA appData;
+
+static uint32_t *ramStart = (uint32_t *)BTL_TRIGGER_RAM_START;
+
+static volatile uint16_t nvmctrlStatus = 0;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_MIPS_Initialize ( void )
+{
+    appData.state = APP_INIT;
+
+    APP_TIMER_START();
+}
+
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_MIPS_Tasks ( void )
+{
+
+    /* Check the application's current state. */
+    switch ( appData.state )
+    {
+        /* Application's initial state. */
+        case APP_INIT:
+        {
+            /* Add Delay for the UART Console to get enumerated after reset */
+            APP_TIMER_DelayMs(1000);
+
+            if ((NVMCON & _NVMCON_PFSWAP_MASK) != _NVMCON_PFSWAP_MASK)
+            {
+                printf("\n\r####### Application running from Program Flash BANK 1 #######\n\r");
+            }
+            else
+            {
+                printf("\n\r####### Application running from Program Flash BANK 2  #######\n\r");
+            }
+
+            printf("\n\r####### Press and Hold the Switch to trigger Bootloader #######\n\r");
+
+            appData.state = APP_SWITCH_PRESS_WAIT;
+            break;
+        }
+
+        case APP_SWITCH_PRESS_WAIT:
+        {
+            if (SWITCH_GET() == SWITCH_PRESSED)
+            {
+                appData.state = APP_TRIGGER_BOOTLOADER;
+            }
+
+            APP_TIMER_DelayMs(1000);
+
+            LED_TOGGLE();
+
+            break;
+        }
+
+        case APP_TRIGGER_BOOTLOADER:
+        {
+            printf("\n\r####### Bootloader Triggered #######\n\r");
+
+            printf("\n\r####### Disconnect console to program new firmware in other Bank from Bootloader #######\n\r");
+
+            ramStart[0] = BTL_TRIGGER_PATTERN;
+            ramStart[1] = BTL_TRIGGER_PATTERN;
+            ramStart[2] = BTL_TRIGGER_PATTERN;
+            ramStart[3] = BTL_TRIGGER_PATTERN;
+
+            APP_SystemReset();
+        }
+
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
+
+
+/*******************************************************************************
+ End of File
+ */
---
+++ apps/uart_fail_safe_bootloader/test_app/firmware/src/config/pic32mz_ef_sk/user.h
@@ -0,0 +1,97 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef USER_H
+#define USER_H
+
+#include "bsp/bsp.h"
+#include "sys/kmem.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#define LED_ON()        LED3_On()
+#define LED_OFF()       LED3_Off()
+#define LED_TOGGLE()    LED3_Toggle()
+
+#define SWITCH_GET()    SWITCH1_Get()
+#define SWITCH_PRESSED  SWITCH1_STATE_PRESSED
+    
+#define APP_TIMER_START     CORETIMER_Start
+#define APP_TIMER_DelayMs   CORETIMER_DelayMs
+
+#define BTL_TRIGGER_RAM_START   KVA0_TO_KVA1(0x80000000)
+
+#define DCACHE_CLEAN_BY_ADDR(start, sz)
+
+static inline void APP_SystemReset( void )
+{
+    /* Perform system unlock sequence */ 
+    SYSKEY = 0x00000000;
+    SYSKEY = 0xAA996655;
+    SYSKEY = 0x556699AA;
+
+    RSWRSTSET = _RSWRST_SWRST_MASK;
+    (void)RSWRST;
+}
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
