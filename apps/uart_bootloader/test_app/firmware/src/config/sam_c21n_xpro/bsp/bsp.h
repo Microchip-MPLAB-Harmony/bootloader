7d5d85054f136909ab1197eb67e6df9ac8214880
Amit Raddi <amit.raddi@microchip.com>
2020-01-30 09:28:15 +0530

    [UART] Regenerated and tested uart_bootloader_test_app_sam_c21n_xpro application configuration
    MH3-29395
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/bsp/bsp.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/bsp/bsp.h
@@ -60,6 +60,7 @@
 // Section: BSP Macros
 // *****************************************************************************
 // *****************************************************************************
+
 /*** LED Macros for LED ***/
 #define LED_Toggle() (PORT_REGS->GROUP[2].PORT_OUTTGL = 1 << 5)
 #define LED_On() (PORT_REGS->GROUP[2].PORT_OUTCLR = 1 << 5)
