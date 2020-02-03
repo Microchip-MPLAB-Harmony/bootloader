6faf1ef015068ed6b1d4c346cfd47fcde85d0fca
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-02-03 09:31:14 +0530

    [I2C_BOOTLOADER] Updated application files
    MH3-29299

--- apps/i2c_bootloader/bootloader_host_nvm/firmware/src/main_e54.c
+++ apps/i2c_bootloader/bootloader_host_nvm/firmware/src/main_e54.c
@@ -65,39 +65,39 @@
 #define SWITCH_GET()                                SWITCH_Get()
 #define SWITCH_PRESSED                              0
 
-#define APP_BL_STATUS_BIT_BUSY                     	(0x01 << 0)
-#define APP_BL_STATUS_BIT_INVALID_COMMAND          	(0x01 << 1)
-#define APP_BL_STATUS_BIT_INVALID_MEM_ADDR         	(0x01 << 2)
-#define APP_BL_STATUS_BIT_COMMAND_EXECUTION_ERROR  	(0x01 << 3)      //Valid only when APP_BL_STATUS_BIT_BUSY is 0
-#define APP_BL_STATUS_BIT_CRC_ERROR                	(0x01 << 4)      
-#define APP_BL_STATUS_BIT_ALL                      	(APP_BL_STATUS_BIT_BUSY | APP_BL_STATUS_BIT_INVALID_COMMAND | APP_BL_STATUS_BIT_INVALID_MEM_ADDR | \
+#define APP_BL_STATUS_BIT_BUSY                      (0x01 << 0)
+#define APP_BL_STATUS_BIT_INVALID_COMMAND           (0x01 << 1)
+#define APP_BL_STATUS_BIT_INVALID_MEM_ADDR          (0x01 << 2)
+#define APP_BL_STATUS_BIT_COMMAND_EXECUTION_ERROR   (0x01 << 3)      //Valid only when APP_BL_STATUS_BIT_BUSY is 0
+#define APP_BL_STATUS_BIT_CRC_ERROR                 (0x01 << 4)
+#define APP_BL_STATUS_BIT_ALL                       (APP_BL_STATUS_BIT_BUSY | APP_BL_STATUS_BIT_INVALID_COMMAND | APP_BL_STATUS_BIT_INVALID_MEM_ADDR | \
                                                     APP_BL_STATUS_BIT_COMMAND_EXECUTION_ERROR | APP_BL_STATUS_BIT_CRC_ERROR)
 
 typedef enum
-{    
+{
     APP_BL_COMMAND_UNLOCK = 0xA0,
-	APP_BL_COMMAND_ERASE,
-    APP_BL_COMMAND_PROGRAM,    
-    APP_BL_COMMAND_VERIFY,
-    APP_BL_COMMAND_RESET,    
-    APP_BL_COMMAND_READ_STATUS,  
-    APP_BL_COMMAND_MAX,
+    APP_BL_COMMAND_ERASE = 0xA1,
+    APP_BL_COMMAND_PROGRAM = 0xA2,
+    APP_BL_COMMAND_VERIFY = 0xA3,
+    APP_BL_COMMAND_RESET = 0xA4,
+    APP_BL_COMMAND_READ_STATUS = 0xA5
 }APP_BL_COMMAND;
 
 typedef enum
 {
     APP_STATE_INIT,
-    APP_STATE_WAIT_SW_PRESS,   
+    APP_STATE_WAIT_SW_PRESS,
     APP_STATE_SEND_UNLOCK_COMMAND,
     APP_STATE_WAIT_UNLOCK_COMMAND_TRANSFER_COMPLETE,
     APP_STATE_SEND_ERASE_COMMAND,
     APP_STATE_WAIT_ERASE_COMMAND_TRANSFER_COMPLETE,
     APP_STATE_SEND_WRITE_COMMAND,
-    APP_STATE_WAIT_WRITE_COMMAND_TRANSFER_COMPLETE,              
+    APP_STATE_WAIT_WRITE_COMMAND_TRANSFER_COMPLETE,
     APP_STATE_SEND_VERIFY_COMMAND,
     APP_STATE_WAIT_VERIFY_COMMAND_TRANSFER_COMPLETE,
     APP_STATE_SEND_RESET_COMMAND,
-    APP_STATE_CHECK_STATUS,    
+    APP_STATE_WAIT_RESET_COMMAND_TRANSFER_COMPLETE,
+    APP_STATE_CHECK_STATUS,
     APP_STATE_SUCCESSFUL,
     APP_STATE_ERROR,
     APP_STATE_IDLE,
@@ -117,13 +117,13 @@ typedef struct
 {
     APP_STATES                      state;
     APP_STATES                      nextState;
-    volatile APP_TRANSFER_STATUS    trasnferStatus;    
+    volatile APP_TRANSFER_STATUS    trasnferStatus;
     uint32_t                        appMemStartAddr;
     uint32_t                        nBytesWritten;
     uint32_t                        nBytesWrittenInErasedPage;
     uint8_t                         status;
-    uint8_t                         wrBuffer[APP_PROGRAM_PAGE_SIZE + APP_PROTOCOL_HEADER_MAX_SIZE];    
-    
+    uint8_t                         wrBuffer[APP_PROGRAM_PAGE_SIZE + APP_PROTOCOL_HEADER_MAX_SIZE];
+
 } APP_DATA;
 
 /* Global Data */
@@ -153,7 +153,7 @@ void APP_I2CEventHandler(uintptr_t context )
 static void APP_Initialize(void)
 {
     appData.appMemStartAddr = APP_IMAGE_START_ADDR;
-    appData.nBytesWritten = 0;        
+    appData.nBytesWritten = 0;
     appData.state = APP_STATE_INIT;
     LED_OFF();
 }
@@ -165,36 +165,36 @@ static uint32_t APP_CRCGenerate(void)
     PAC_PeripheralProtectSetup (PAC_PERIPHERAL_DSU, PAC_PROTECTION_CLEAR);
 
     DSU_CRCCalculate (
-		(uint32_t)&image_pattern[0], 
-		APP_IMAGE_SIZE, 
-		0xffffffff, 
-		&crc
-	);
+        (uint32_t)&image_pattern[0],
+        APP_IMAGE_SIZE,
+        0xffffffff,
+        &crc
+    );
 
     return crc;
 }
 
 static uint32_t APP_ImageDataWrite(uint32_t memAddr, uint32_t nBytes)
 {
-    uint32_t k; 
-    uint32_t nTxBytes = 0;       
+    uint32_t k;
+    uint32_t nTxBytes = 0;
     uint32_t wrIndex = (memAddr - APP_IMAGE_START_ADDR);
-    
+
     appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_PROGRAM;
     appData.wrBuffer[nTxBytes++] = (nBytes >> 24);
     appData.wrBuffer[nTxBytes++] = (nBytes >> 16);
     appData.wrBuffer[nTxBytes++] = (nBytes >> 8);
-    appData.wrBuffer[nTxBytes++] = (nBytes);   
+    appData.wrBuffer[nTxBytes++] = (nBytes);
     appData.wrBuffer[nTxBytes++] = (memAddr >> 24);
     appData.wrBuffer[nTxBytes++] = (memAddr >> 16);
     appData.wrBuffer[nTxBytes++] = (memAddr >> 8);
     appData.wrBuffer[nTxBytes++] = (memAddr);
-    
+
 
     for (k = 0; k < nBytes; k++, nTxBytes++)
     {
         appData.wrBuffer[nTxBytes] = image_pattern[wrIndex + k];
-    }        
+    }
 
     return nTxBytes;
 }
@@ -202,43 +202,43 @@ static uint32_t APP_ImageDataWrite(uint32_t memAddr, uint32_t nBytes)
 static uint32_t APP_UnlockCommandSend(uint32_t appStartAddr, uint32_t appSize)
 {
     uint32_t nTxBytes = 0;
-    
-    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_UNLOCK;  
+
+    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_UNLOCK;
     appData.wrBuffer[nTxBytes++] = (appStartAddr >> 24);
     appData.wrBuffer[nTxBytes++] = (appStartAddr >> 16);
     appData.wrBuffer[nTxBytes++] = (appStartAddr >> 8);
-    appData.wrBuffer[nTxBytes++] = (appStartAddr);      
+    appData.wrBuffer[nTxBytes++] = (appStartAddr);
     appData.wrBuffer[nTxBytes++] = (appSize >> 24);
     appData.wrBuffer[nTxBytes++] = (appSize >> 16);
     appData.wrBuffer[nTxBytes++] = (appSize >> 8);
     appData.wrBuffer[nTxBytes++] = (appSize);
-    
+
     return nTxBytes;
 }
 
 static uint32_t APP_VerifyCommandSend(uint32_t crc)
 {
     uint32_t nTxBytes = 0;
-    
-    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_VERIFY;  
+
+    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_VERIFY;
     appData.wrBuffer[nTxBytes++] = (crc >> 24);
     appData.wrBuffer[nTxBytes++] = (crc >> 16);
     appData.wrBuffer[nTxBytes++] = (crc >> 8);
-    appData.wrBuffer[nTxBytes++] = (crc);      
-    
+    appData.wrBuffer[nTxBytes++] = (crc);
+
     return nTxBytes;
 }
 
 static uint32_t APP_EraseCommandSend(uint32_t memAddr)
 {
     uint32_t nTxBytes = 0;
-    
-    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_ERASE;  
+
+    appData.wrBuffer[nTxBytes++] = APP_BL_COMMAND_ERASE;
     appData.wrBuffer[nTxBytes++] = (memAddr >> 24);
     appData.wrBuffer[nTxBytes++] = (memAddr >> 16);
     appData.wrBuffer[nTxBytes++] = (memAddr >> 8);
-    appData.wrBuffer[nTxBytes++] = (memAddr);      
-    
+    appData.wrBuffer[nTxBytes++] = (memAddr);
+
     return nTxBytes;
 }
 
@@ -248,14 +248,14 @@ static uint32_t APP_EraseCommandSend(uint32_t memAddr)
 // *****************************************************************************
 // *****************************************************************************
 int main ( void )
-{    
+{
     uint32_t nTxBytes;
     uint32_t crc;
-    
+
     APP_Initialize();
-    
+
     /* Initialize all modules */
-    SYS_Initialize ( NULL );    
+    SYS_Initialize ( NULL );
 
     while(1)
     {
@@ -264,23 +264,23 @@ int main ( void )
             case APP_STATE_INIT:
                 SERCOM7_I2C_CallbackRegister( APP_I2CEventHandler, (uintptr_t)&appData.trasnferStatus );
                 appData.state = APP_STATE_WAIT_SW_PRESS;
-                break;                            
-                
+                break;
+
             case APP_STATE_WAIT_SW_PRESS:
                 if (SWITCH_GET() == SWITCH_PRESSED)
-                {                    
+                {
                     appData.state = APP_STATE_SEND_UNLOCK_COMMAND;
                 }
-                break;            
+                break;
 
             case APP_STATE_SEND_UNLOCK_COMMAND:
-                
+
                 nTxBytes = APP_UnlockCommandSend(APP_IMAGE_START_ADDR, APP_IMAGE_SIZE);
                 appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
                 SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], nTxBytes);
-                appData.state = APP_STATE_WAIT_UNLOCK_COMMAND_TRANSFER_COMPLETE;               
+                appData.state = APP_STATE_WAIT_UNLOCK_COMMAND_TRANSFER_COMPLETE;
                 break;
-                
+
             case APP_STATE_WAIT_UNLOCK_COMMAND_TRANSFER_COMPLETE:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
@@ -300,23 +300,23 @@ int main ( void )
                         appData.nBytesWrittenInErasedPage = 0;
                         appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
                         SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], nTxBytes);
-                        appData.state = APP_STATE_WAIT_ERASE_COMMAND_TRANSFER_COMPLETE;                        
+                        appData.state = APP_STATE_WAIT_ERASE_COMMAND_TRANSFER_COMPLETE;
                     }
                 }
                 else
                 {
-                    /* Firmware programming complete. Now verify CRC by sending verify command */                                
+                    /* Firmware programming complete. Now verify CRC by sending verify command */
                     appData.state = APP_STATE_SEND_VERIFY_COMMAND;
                 }
                 break;
-                
+
             case APP_STATE_WAIT_ERASE_COMMAND_TRANSFER_COMPLETE:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
                     /* Read the status of the erase command */
                     appData.wrBuffer[0] = APP_BL_COMMAND_READ_STATUS;
                     appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);                    
+                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);
                     appData.state = APP_STATE_CHECK_STATUS;
                     appData.nextState = APP_STATE_SEND_WRITE_COMMAND;
                 }
@@ -324,33 +324,33 @@ int main ( void )
                 {
                     appData.state = APP_STATE_ERROR;
                 }
-                
+
                 break;
-                
-            case APP_STATE_SEND_WRITE_COMMAND:  
+
+            case APP_STATE_SEND_WRITE_COMMAND:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
-                    nTxBytes = APP_ImageDataWrite((appData.appMemStartAddr + appData.nBytesWritten), APP_PROGRAM_PAGE_SIZE);                                      
+                    nTxBytes = APP_ImageDataWrite((appData.appMemStartAddr + appData.nBytesWritten), APP_PROGRAM_PAGE_SIZE);
                     appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
                     SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], nTxBytes);
                     appData.state = APP_STATE_WAIT_WRITE_COMMAND_TRANSFER_COMPLETE;
-                }                
+                }
                 else if (appData.trasnferStatus == APP_TRANSFER_STATUS_ERROR)
                 {
                     appData.state = APP_STATE_ERROR;
-                }                                                               
+                }
                 break;
 
             case APP_STATE_WAIT_WRITE_COMMAND_TRANSFER_COMPLETE:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
-                    appData.nBytesWritten += APP_PROGRAM_PAGE_SIZE;  
+                    appData.nBytesWritten += APP_PROGRAM_PAGE_SIZE;
                     appData.nBytesWrittenInErasedPage += APP_PROGRAM_PAGE_SIZE;
-                    
+
                     /* Read the status of the write command */
                     appData.wrBuffer[0] = APP_BL_COMMAND_READ_STATUS;
                     appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);                    
+                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);
                     appData.state = APP_STATE_CHECK_STATUS;
                     if (appData.nBytesWrittenInErasedPage == APP_ERASE_PAGE_SIZE)
                     {
@@ -367,23 +367,23 @@ int main ( void )
                 {
                     appData.state = APP_STATE_ERROR;
                 }
-                break;                            
-                                     
-            case APP_STATE_SEND_VERIFY_COMMAND: 
+                break;
+
+            case APP_STATE_SEND_VERIFY_COMMAND:
                 crc = APP_CRCGenerate();
-                nTxBytes = APP_VerifyCommandSend(crc);                 
+                nTxBytes = APP_VerifyCommandSend(crc);
                 appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], nTxBytes);                    
+                SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], nTxBytes);
                 appData.state = APP_STATE_WAIT_VERIFY_COMMAND_TRANSFER_COMPLETE;
                 break;
-                
+
             case APP_STATE_WAIT_VERIFY_COMMAND_TRANSFER_COMPLETE:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
                     /* Read the status of the verify command */
                     appData.wrBuffer[0] = APP_BL_COMMAND_READ_STATUS;
                     appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);        
+                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);
                     appData.state = APP_STATE_CHECK_STATUS;
                     appData.nextState = APP_STATE_SEND_RESET_COMMAND;
                 }
@@ -392,7 +392,7 @@ int main ( void )
                     appData.state = APP_STATE_ERROR;
                 }
                 break;
-                
+
             case APP_STATE_CHECK_STATUS:
                 if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
                 {
@@ -403,40 +403,51 @@ int main ( void )
                     else
                     {
                         appData.state = appData.nextState;
-                    }                    
+                    }
                 }
                 else if (appData.trasnferStatus == APP_TRANSFER_STATUS_ERROR)
-                {                     
+                {
                     /* Slave is busy. Keep checking the status */
                     appData.wrBuffer[0] = APP_BL_COMMAND_READ_STATUS;
                     appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);  
+                    SERCOM7_I2C_WriteRead(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1,  &appData.status, 1);
                 }
                 break;
 
             case APP_STATE_SEND_RESET_COMMAND:
-                
+
                 appData.wrBuffer[0] = APP_BL_COMMAND_RESET;
                 appData.trasnferStatus = APP_TRANSFER_STATUS_IN_PROGRESS;
-                SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1);                    
-                appData.state = APP_STATE_SUCCESSFUL;                        
+                SERCOM7_I2C_Write(APP_I2C_SLAVE_ADDR, &appData.wrBuffer[0], 1);
+                appData.state = APP_STATE_WAIT_RESET_COMMAND_TRANSFER_COMPLETE;
                 break;
-                
+
+            case APP_STATE_WAIT_RESET_COMMAND_TRANSFER_COMPLETE:
+                if (appData.trasnferStatus == APP_TRANSFER_STATUS_SUCCESS)
+                {
+                    appData.state = APP_STATE_SUCCESSFUL;
+                }
+                else if (appData.trasnferStatus == APP_TRANSFER_STATUS_ERROR)
+                {
+                    appData.state = APP_STATE_ERROR;
+                }
+                break;
+
             case APP_STATE_SUCCESSFUL:
-            
-                LED_ON();       
+
+                LED_ON();
                 appData.state = APP_STATE_IDLE;
                 break;
-            
+
             case APP_STATE_ERROR:
-            
+
                 LED_OFF();
                 appData.state = APP_STATE_IDLE;
                 break;
-                
+
             case APP_STATE_IDLE:
                 break;
-            
+
             default:
                 break;
         }
